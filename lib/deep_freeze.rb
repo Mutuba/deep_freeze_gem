8793a24e74c64b7ea290c432889b839890393cce
Mutuba <danielmutubait@gmail.com>
2023-08-22 15:56:57 +0300

    Fix cyclic loop

--- lib/deep_freeze.rb
+++ lib/deep_freeze.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 require_relative 'deep_freeze/version'
-# DeepFreeze module
-module DeepFreeze
-  def deep_freeze(obj)
-    case obj
 
+module DeepFreeze
+  def deep_freeze(obj, frozen_objects = {})
+    return obj if obj.frozen? || frozen_objects.key?(obj)
+
+    frozen_objects[obj] = true
+
+    case obj
     when String, Numeric, Symbol, NilClass, FalseClass, TrueClass
       obj.freeze
     when Hash
       obj.each do |key, value|
-        deep_freeze(key)
-        deep_freeze(value)
+        deep_freeze(key, frozen_objects)
+        deep_freeze(value, frozen_objects)
       end
       obj.freeze
     when Array
-      obj.each { |value| deep_freeze(value) }
+      obj.each { |value| deep_freeze(value, frozen_objects) }
       obj.freeze
     else
       raise "Can't deep_freeze objects of type #{obj.class}"
